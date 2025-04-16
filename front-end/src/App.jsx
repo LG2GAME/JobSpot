72085dfcb31da9cb7f95295b869f192b6aaf6649
lg2game <you@example.com>
2025-04-16 22:45:44 +0200

    Add navigation globaly

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "@routes/AppRoutes";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigation } from "@components/layout/navigation/Navigation";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "@styles/main.scss";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigation />
       <AppRoutes />
     </BrowserRouter>
   );
