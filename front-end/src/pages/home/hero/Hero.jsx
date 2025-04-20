c568bc845c1cc9cc9df9397573e98daa927ae633
lg2game <you@example.com>
2025-04-20 20:11:35 +0200

    add hero

---
+++ front-end/src/pages/home/hero/Hero.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+import "./Hero.scss";
+import learningPanaImage from "@assets/images/learning-pana.png";
+
+export const Hero = () => (
+  <section className="hero">
+    <div className="hero__content">
+      <h1 className="hero-text">
+        <span className="hero-text hero-text--orange">Znalezienie pracy</span>,
+        nigdy nie było tak łatwe.
+      </h1>
+      <p className="side-text">
+        Prosty proces aplikacji i dostęp do najlepszych ofert pracy – znajdź
+        idealną posadę w mgnieniu oka!
+      </p>
+      <div className="hero__content-buttons">
+        <Link to="/" className="btn btn-white">
+          Dołącz za darmo!
+        </Link>
+        <Link to="/" className="btn btn-w30 button-text">
+          Znajdź pracę
+        </Link>
+      </div>
+    </div>
+    <div className="hero__image">
+      <img src={learningPanaImage} alt="" />
+    </div>
+  </section>
+);
