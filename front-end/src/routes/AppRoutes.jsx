2885968f10bc2df42efd58962a7a1c8a53508d71
lg2game <you@example.com>
2025-04-20 20:10:47 +0200

    add home to routs

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -1,16 +1,36 @@
-// const Home = lazy(() => import("@pages/home/Home"));
-
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+
+import Backdrop from "../components/layout/backdrop/Backdrop";
+
+const Home = lazy(() => import("@pages/home/Home"));
 
 const AppRoutes = () => {
   return (
     <Suspense fallback={<div>Ładowanie...</div>}>
       <Routes>
-        <Route path="/" element={<div></div>} />
+        <Route
+          path="/"
+          element={
+            <BackdropWrapper>
+              <Home />
+            </BackdropWrapper>
+          }
+        />
       </Routes>
     </Suspense>
   );
 };
+
+const ContainerWrapper = ({ children }) => (
+  <div className="container">{children}</div>
+);
+
+const BackdropWrapper = ({ children }) => (
+  <>
+    <Backdrop />
+    <ContainerWrapper>{children}</ContainerWrapper>
+  </>
+);
 
 export default AppRoutes;
