873af1cd2d001caa45a6012e67bed9a165a7aad5
lg2game <you@example.com>
2025-04-21 20:33:12 +0200

    Add footer to render

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,12 +4,14 @@ import AppRoutes from "@routes/AppRoutes";
 import { Navigation } from "@components/layout/navigation/Navigation";
 
 import "@styles/main.scss";
+import { Footer } from "./components/layout/footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
       <AppRoutes />
+      <Footer />
     </BrowserRouter>
   );
 }
