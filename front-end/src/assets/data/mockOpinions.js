001ebdf394ea31ed6cc0cd650bb4dcd4b1a77d5a
lg2game <you@example.com>
2025-04-21 20:33:26 +0200

    create mock data

---
+++ front-end/src/assets/data/mockOpinions.js
@@ -0,0 +1,26 @@
+export const mockOpinions = [
+  {
+    image:
+      "https://marketplace.canva.com/EAF_Tt9YT4s/2/0/1600w/canva-niebieskie-%C5%BC%C3%B3%C5%82te-proste-profesjonalne-zdj%C4%99cie-profilowe-na-instagram-TuMO7MFjUlY.jpg",
+    name: "Jan Kowalski",
+    job: "Projektant graficzny",
+    comment:
+      "Dzięki tej platformie szybko znalazłem pracę, która idealnie pasowała do moich umiejętności. Aplikowanie jest naprawdę łatwe, a oferty są aktualne!",
+  },
+  {
+    image:
+      "https://marianchrzan.pl/storage/2023/07/Zdjecie-profilowe_czarne_tlo.jpeg",
+    name: "Janina Kowalska",
+    job: "Projektant graficzny",
+    comment:
+      "To najlepsze narzędzie do szukania pracy, jakie znalazłem! Dzięki prostemu procesowi aplikacji i spersonalizowanym ofertom znalazłem wymarzoną posadę.",
+  },
+  {
+    image:
+      "https://marszalstudio.pl/wp-content/uploads/2024/01/fajne-zdjecia-profilowe-12.webp",
+    name: "Jan Kowalski",
+    job: "Projektant graficzny",
+    comment:
+      "Jestem bardzo zadowolony z platformy! Pomogła mi znaleźć świetną ofertę pracy i przygotować profesjonalne CV, które zrobiło wrażenie na pracodawcy.",
+  },
+];
