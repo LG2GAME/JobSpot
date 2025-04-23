94045706f6c26ab138deb1bf8eadf7183057fe54
lg2game <you@example.com>
2025-04-23 22:39:26 +0200

    add datas

---
+++ front-end/src/assets/data/filtersOptions.js
@@ -0,0 +1,39 @@
+export const filtersOptions = [
+  {
+    title: "Poziom stanowiska",
+    options: [
+      "praktykant / stażysta",
+      "junior",
+      "mid / regular",
+      "senior",
+      "ekspert",
+      "kierownik",
+      "menedżer",
+    ],
+  },
+  {
+    title: "Rodzaj umowy",
+    options: ["umowa o pracę", "umowa o dzieło", "umowa zlecenie", "B2B"],
+  },
+  {
+    title: "Wynagrodzenie",
+    options: ["Pokoaż oferty z widełkami"],
+  },
+  {
+    title: "Wymiar pracy",
+    options: ["część etatu", "dodatkowa", "pełny etat"],
+  },
+  {
+    title: "Tryb pracy",
+    options: [
+      "praca stacjonarna",
+      "praca hybrydowa",
+      "praca zdalna",
+      "praca mobilna",
+    ],
+  },
+  {
+    title: "Czas publikacji",
+    options: ["24h", "7 dni", "14 dni", "30 dni"],
+  },
+];
