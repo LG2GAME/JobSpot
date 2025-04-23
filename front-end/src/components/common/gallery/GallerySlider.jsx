3435fc4b9a538db9aa21d25579f76e1a3687d44f
lg2game <you@example.com>
2025-04-23 22:41:34 +0200

    edit gallery slider

--- front-end/src/components/common/gallery/GallerySlider.jsx
+++ front-end/src/components/common/gallery/GallerySlider.jsx
@@ -1,42 +1,11 @@
-import { Link } from "react-router-dom";
-
-import { Card } from "@components/common/cards/Card";
+import { JobCard } from "@components/common/cards/Card";
 
 import "./GallerySlider.scss";
-import localisationIcon from "@assets/images/localisation.png";
-import timeJobIcon from "@assets/images/time-job.png";
 
-export const GallerySlider = ({ data, scrollRef }) => (
+export const GallerySlider = ({ data, scrollRef, length }) => (
   <div className="job-slider" ref={scrollRef}>
-    {data.map((item, index) => (
-      <Card key={index}>
-        <div className="job-card__header">
-          <img src={item.image} alt={item.title} />
-          <div className="job-card__title-box">
-            <p className="card-header">{item.title}</p>
-            <div className="job-card__info">
-              <p className="card-side-text">{item.comp}</p>
-              <p className="card-side-text">{item.time}</p>
-            </div>
-          </div>
-        </div>
-        <p className="card-side-text">{item.desc}</p>
-        <div className="job-card__footer">
-          <Link to="/" className="btn btn-white-br">
-            Aplikuj
-          </Link>
-          <span>
-            <div className="job-card__footer-info">
-              <img src={localisationIcon} alt="lokalizacja" />
-              <p className="card-side-text">{item.localisation}</p>
-            </div>
-            <div className="job-card__footer-info">
-              <img src={timeJobIcon} alt="czas pracy" />
-              <p className="card-side-text">{item.timeJob}</p>
-            </div>
-          </span>
-        </div>
-      </Card>
+    {data.slice(0, length).map((offer, index) => (
+      <JobCard key={index} offer={offer} />
     ))}
   </div>
 );
