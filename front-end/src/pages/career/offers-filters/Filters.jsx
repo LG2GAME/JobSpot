cccc73c29000cf4ae8fb009e8fb559694b5a794f
lg2game <you@example.com>
2025-04-24 22:34:58 +0200

    add arrow to header

--- front-end/src/pages/career/offers-filters/Filters.jsx
+++ front-end/src/pages/career/offers-filters/Filters.jsx
@@ -3,14 +3,20 @@ import { Dropdown } from "@components/common/dropdown/Dropdown";
 import { filtersOptions } from "@assets/data/filtersOptions";
 
 import "./OffersFilters.scss";
+import arrowIcon from "@assets/images/up-arrow.png";
 
 export const Filters = ({ collapsed, setCollapsed }) => (
   <div
     className={`career-filters ${collapsed ? "career-filters--collapsed" : ""}`}
   >
-    <h1 className="hero-text" onClick={() => setCollapsed(!collapsed)}>
-      Filtry
-    </h1>
+    <div
+      className="career-filters__header"
+      onClick={() => setCollapsed(!collapsed)}
+    >
+      <h1 className="hero-text">Filtry</h1>
+      <img src={arrowIcon} alt="arrow" />
+    </div>
+
     <Dropdown data={filtersOptions} />
   </div>
 );
