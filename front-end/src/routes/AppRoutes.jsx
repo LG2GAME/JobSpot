a1ebc44301ee82cc2e443bcd7dfb278abb29555e
lg2game <you@example.com>
2025-04-24 22:35:11 +0200

    add new routes

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Backdrop, {
-  SmallBackdrop,
-} from "../components/layout/backdrop/Backdrop";
+import Backdrop, { SmallBackdrop } from "@components/layout/backdrop/Backdrop";
 
 const Home = lazy(() => import("@pages/home/Home"));
 const Career = lazy(() => import("@pages/career/Career"));
+const Blog = lazy(() => import("@pages/blog/Blog"));
+const AboutUs = lazy(() => import("@pages/about-us/AboutUs"));
+const Login = lazy(() => import("@pages/auth/login/Login"));
+const Register = lazy(() => import("@pages/auth/register/Register"));
 
 const AppRoutes = () => {
   return (
@@ -26,6 +28,38 @@ const AppRoutes = () => {
             <SmallBackdropWrapper>
               <Career />
             </SmallBackdropWrapper>
+          }
+        />
+        <Route
+          path="/blog"
+          element={
+            <SmallBackdropWrapper>
+              <Blog />
+            </SmallBackdropWrapper>
+          }
+        />
+        <Route
+          path="/about-us"
+          element={
+            <SmallBackdropWrapper>
+              <AboutUs />
+            </SmallBackdropWrapper>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <ContainerWrapper>
+              <Login />
+            </ContainerWrapper>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <ContainerWrapper>
+              <Register />
+            </ContainerWrapper>
           }
         />
       </Routes>
