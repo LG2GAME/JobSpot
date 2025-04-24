997ddf17c4fd21c030212cba41aabc4ac638788a
lg2game <you@example.com>
2025-04-24 22:33:46 +0200

    create empty sections

---
+++ front-end/src/pages/about-us/AboutUs.jsx
@@ -0,0 +1,10 @@
+import { TitleContainer } from "@components/common/headers/TitleContainer";
+
+export default function AboutUs() {
+  return (
+    <TitleContainer>
+      <h1 className="page-title">O nas</h1>
+      <p>Może kiedyś coś tu będzie (raczej nie)</p>
+    </TitleContainer>
+  );
+}
---
+++ front-end/src/pages/blog/Blog.jsx
@@ -0,0 +1,10 @@
+import { TitleContainer } from "@components/common/headers/TitleContainer";
+
+export default function Blog() {
+  return (
+    <TitleContainer>
+      <h1 className="page-title">blog</h1>
+      <p>Może kiedyś coś tu będzie (raczej nie)</p>
+    </TitleContainer>
+  );
+}
