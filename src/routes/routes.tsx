44c27f093223d0001f83d8a0453b7cb95695a2e6
lg2game <you@example.com>
2025-11-08 19:38:25 +0100

    chore: update routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "@components/layout/Layout";
+import { Home } from "@pages/index";
 
 export const router = createBrowserRouter([
   {
     element: <Layout />,
-    children: [{ path: "/", element: <></> }],
+    children: [{ path: "/", element: <Home /> }],
   },
 ]);
