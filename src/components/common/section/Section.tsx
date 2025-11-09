1f198b8bc2e25358ed2fefc2295c47cfd28a99d1
lg2game <you@example.com>
2025-11-09 15:44:48 +0100

    chore: add version (type)

--- src/components/common/section/Section.tsx
+++ src/components/common/section/Section.tsx
@@ -3,10 +3,13 @@ import * as style from "./section.css";
 
 interface SectionProps {
   children: ReactNode;
+  type?: "row" | "column";
 }
 
-const Section = ({ children }: SectionProps) => {
-  return <section className={style.section}>{children}</section>;
+const Section = ({ children, type = "row" }: SectionProps) => {
+  return (
+    <section className={style.section({ type: type })}>{children}</section>
+  );
 };
 
 export default Section;
--- src/components/common/section/section.css.ts
+++ src/components/common/section/section.css.ts
@@ -1,17 +1,24 @@
 import { vars } from "@styles/theme.css";
-import { style } from "@vanilla-extract/css";
+import { recipe } from "@vanilla-extract/recipes";
 
-export const section = style({
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "space-between",
-  alignItems: "center",
-  minHeight: "100vh",
-  height: "100%",
-  selectors: {
-    "&:first-of-type": {
-      minHeight: `calc(100vh - var(--nav-height) - ${vars.spacing.padding.lg})`,
-      height: `calc(100vh - var(--nav-height) - ${vars.spacing.padding.lg})`,
+export const section = recipe({
+  base: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "100%",
+    padding: `${vars.spacing.padding.lg}`,
+    selectors: {
+      "&:first-of-type": {
+        minHeight: `calc(100vh - var(--nav-height) - ${vars.spacing.padding.sm})`,
+        height: `calc(100vh - var(--nav-height) - ${vars.spacing.padding.sm})`,
+      },
+    },
+  },
+  variants: {
+    type: {
+      row: { flexDirection: "row" },
+      column: { flexDirection: "column", gap: vars.spacing.gap.lg },
     },
   },
 });
