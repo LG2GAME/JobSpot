2eade876e9c8865ddbab1c57eca50dda4a7de65f
lg2game <you@example.com>
2025-11-09 15:45:13 +0100

    chore: update home exports

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,13 @@
+import GetStarted from "./getStarted/GetStarted";
 import Hero from "./hero/Hero";
+import JobCategories from "./jobCategories/JobCategories";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <GetStarted />
+      <JobCategories />
     </>
   );
 };
