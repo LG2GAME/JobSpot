7668895367d52dcc27b31beaf978daac60ad7b9d
lg2game <you@example.com>
2025-11-14 23:12:59 +0100

    chore: add new exports

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,3 +3,4 @@ export { default as Section } from "./section/Section";
 export { default as Card } from "./card/Card";
 export { default as CardWrapper } from "./cardWrapper/CardWrapper";
 export { default as Headline, Highlight } from "./headline/Headline";
+export { default as Track } from "./track/Track";
