9ee6973dcc96c5e9fb8a5739a8f7c13e3fd77388
lg2game <you@example.com>
2025-11-15 21:48:02 +0100

    chore: add 404 and in progress to routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,8 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "@components/layout/Layout";
-import { Home } from "@pages/index";
+import { Layout, ErrorLayout } from "@components/layout";
+import { Home, NotFound } from "@pages/index";
+import UnderConstruction from "@pages/underConstruction/UnderConstruction";
+
+export const plannedRoutes = [
+  "/career",
+  "/blog",
+  "/about-us",
+  "/account",
+  "/terms",
+  "/privacy-policy",
+  "/cookies",
+];
 
 export const router = createBrowserRouter([
+  {
+    element: <ErrorLayout />,
+    children: [{ path: "*", element: <NotFound /> }],
+  },
+  ...plannedRoutes.map((path) => ({
+    element: <ErrorLayout />,
+    children: [{ path: path, element: <UnderConstruction /> }],
+  })),
   {
     element: <Layout />,
     children: [{ path: "/", element: <Home /> }],
