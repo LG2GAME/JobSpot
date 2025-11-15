37b3418dc5325e8cfad522d959f06f060b9bca8e
lg2game <you@example.com>
2025-11-15 15:06:04 +0100

    chore: progress line

--- src/components/features/opinionsSlider/components/sliderProgress/SliderProgress.tsx
+++ src/components/features/opinionsSlider/components/sliderProgress/SliderProgress.tsx
@@ -7,15 +7,17 @@ interface SliderProgressProps {
 
 const SliderProgress = ({ offset, totalPages }: SliderProgressProps) => {
   return (
-    <div className={style.sliderProgressWrapper}>
-      {Array.from({ length: totalPages }).map((_, index) => (
-        <div
-          key={index}
-          className={style.sliderProgressDot({
-            state: index === offset ? "active" : "inactive",
-          })}
-        />
-      ))}
+    <div className={style.sliderProgressContainer}>
+      <div className={style.sliderProgressWrapper}>
+        {Array.from({ length: totalPages }).map((_, index) => (
+          <div
+            key={index}
+            className={style.sliderProgressDot({
+              state: index === offset ? "active" : "inactive",
+            })}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/features/opinionsSlider/components/sliderProgress/sliderProgress.css.ts
+++ src/components/features/opinionsSlider/components/sliderProgress/sliderProgress.css.ts
@@ -2,11 +2,24 @@ import { vars } from "@styles/theme.css";
 import { style } from "@vanilla-extract/css";
 import { recipe } from "@vanilla-extract/recipes";
 
+export const sliderProgressContainer = style({
+  display: "flex",
+  justifyContent: "center",
+});
+
 export const sliderProgressWrapper = style({
   display: "flex",
   gap: vars.spacing.gap.xxs,
-  justifyContent: "center",
   alignItems: "center",
+  position: "relative",
+  "::before": {
+    content: "",
+    position: "absolute",
+    width: "100%",
+    height: 3,
+    backgroundColor: vars.color.orange,
+    zIndex: -1,
+  },
 });
 
 export const sliderProgressDot = recipe({
@@ -17,7 +30,10 @@ export const sliderProgressDot = recipe({
   },
   variants: {
     state: {
-      active: { border: `2px solid ${vars.color.orange}` },
+      active: {
+        border: `3px solid ${vars.color.orange}`,
+        backgroundColor: vars.color.white,
+      },
       inactive: { backgroundColor: vars.color.orange },
     },
   },
