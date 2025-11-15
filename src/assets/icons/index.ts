490edf02a062c72260820d6d2d0bdeca36864a25
lg2game <you@example.com>
2025-11-15 15:03:33 +0100

    chore: add assets

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,9 +1,23 @@
 import search from "./search.svg";
 import jobLocalisation from "./localisation.png";
 import jobTime from "./time-job.png";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import youtube from "./youtube.svg";
+import gps from "./gps.svg";
+import phone from "./phone.svg";
+import email from "./email.svg";
 
 export const icon = {
   search,
   jobLocalisation,
   jobTime,
+  facebook,
+  instagram,
+  linkedin,
+  youtube,
+  gps,
+  phone,
+  email,
 };
