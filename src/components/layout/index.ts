01a567992d19840945760e534e3cac2cfc4f80ed
lg2game <you@example.com>
2025-11-15 15:06:39 +0100

    chore: add export

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,3 +1,4 @@
 export { default as Navigation } from "./navigation/Navigation";
 export { default as Backdrop } from "./backdrop/Backdrop";
 export { default as PageLayout } from "./Layout";
+export { default as Footer } from "./footer/Footer";
