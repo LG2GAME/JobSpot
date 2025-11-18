c20047cabbe10a48149b98fc5035080b977392ca
lg2game <you@example.com>
2025-11-18 22:56:55 +0100

    chore: update routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,29 +1,86 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Layout, ErrorLayout } from "@components/layout";
-import { Home, NotFound } from "@pages/index";
-import UnderConstruction from "@pages/underConstruction/UnderConstruction";
+import {
+  Layout,
+  ErrorLayout,
+  AuthLayout,
+  AccountLayout,
+} from "@components/layout";
+import {
+  Account,
+  Home,
+  Login,
+  NotFound,
+  Register,
+  UnderConstruction,
+} from "@pages/index";
+
+// src/router.tsx
 
 export const plannedRoutes = [
   "/career",
   "/blog",
   "/about-us",
-  "/account",
   "/terms",
   "/privacy-policy",
   "/cookies",
+  "/account/forgot-password",
 ];
 
 export const router = createBrowserRouter([
+  // GRUPA 0: TRASY BŁĘDÓW I W BUDOWIE (globalne)
   {
     element: <ErrorLayout />,
-    children: [{ path: "*", element: <NotFound /> }],
+    children: [
+      { path: "*", element: <NotFound /> },
+      ...plannedRoutes.map((path) => ({
+        path: path,
+        element: <UnderConstruction />,
+      })),
+    ],
   },
-  ...plannedRoutes.map((path) => ({
-    element: <ErrorLayout />,
-    children: [{ path: path, element: <UnderConstruction /> }],
-  })),
+
+  // ------------------------------------------------------------------
+  // GRUPA 1: TRASY GŁÓWNE (PUBLICZNY LAYOUT Z HEADER/FOOTER)
+  // ------------------------------------------------------------------
   {
     element: <Layout />,
     children: [{ path: "/", element: <Home /> }],
+  },
+
+  // ------------------------------------------------------------------
+  // GRUPA 2: TRASY AUTORYZACJI (LOGOWANIE/REJESTRACJA)
+  // ------------------------------------------------------------------
+  {
+    path: "/account",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+
+  // ------------------------------------------------------------------
+  // GRUPA 3: TRASY PRYWATNE (PANEL KONTA/SIDEBAR)
+  // Wymagają tu dodatkowego Guarda (PrivateRoute) - PAMIĘTAJ O NIM!
+  // ------------------------------------------------------------------
+  {
+    path: "/account",
+    element: <AccountLayout />,
+    children: [
+      {
+        index: true,
+        element: <Account />,
+      },
+      {
+        path: "settings",
+        element: <Account />,
+      },
+    ],
   },
 ]);
