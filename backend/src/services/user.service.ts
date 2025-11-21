e464333214f1f983c4d655b9da4fa054318e6487
lg2game <you@example.com>
2025-11-21 22:06:02 +0100

    feat: menage user (log/reg)

---
+++ backend/src/services/user.service.ts
@@ -0,0 +1,66 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+interface User {
+  email: string;
+  password: string;
+}
+
+export type SafeUser = {
+  id: number;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type AuthResponse = {
+  user: SafeUser;
+  token: string;
+};
+
+export async function registerUser({ email, password }: User) {
+  const existingUser = await prisma!.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    throw new Error("User with this email already exists.");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await prisma!.user.create({
+    data: {
+      email: email,
+      password: hashedPassword,
+    },
+  });
+
+  const { password: _password, ...safeUser } = newUser;
+
+  return safeUser;
+}
+
+export async function loginUser({
+  email,
+  password,
+}: User): Promise<AuthResponse> {
+  const user = await prisma!.user.findUnique({ where: { email } });
+
+  if (user == null) {
+    throw new Error("Invalid credentials.");
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    throw new Error("Invalid credentials.");
+  }
+
+  const { password: _password, ...safeUser } = user;
+
+  const token = jwt.sign(safeUser, process.env.JWT_SECRET!, {
+    expiresIn: "1h",
+  });
+  return { user: safeUser as SafeUser, token };
+}
