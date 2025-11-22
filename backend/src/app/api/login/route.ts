37cbfdcf81fa84d5e9d2ecf595f9a94f41c75cd8
lg2game <you@example.com>
2025-11-22 23:33:02 +0100

    fix: add login validation

--- backend/src/app/api/login/route.ts
+++ backend/src/app/api/login/route.ts
@@ -1,10 +1,22 @@
 import { loginUser } from "@/services/user.service";
 import { NextResponse } from "next/server";
 import { createAuthCookieHeader } from "@/utils/cookie.util";
+import { validateLogin } from "@/utils/validation";
 
 export async function POST(request: Request) {
   try {
     const { email, password } = await request.json();
+    const validationError = validateLogin({ email, password });
+    if (validationError != null) {
+      return NextResponse.json(
+        {
+          field: validationError.field,
+          message: validationError.message,
+        },
+        { status: validationError.status }
+      );
+    }
+
     const { user, token } = await loginUser({ email, password });
 
     const cookie = createAuthCookieHeader(token);
